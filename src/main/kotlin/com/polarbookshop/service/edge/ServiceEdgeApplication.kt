91df3c8dcb70230b76d64a221ba9f505183f3599
dmitry.ivzhenko <demi-drolll@yandex.ru>
2024-01-23 16:54:02 +0300

    init

---
+++ src/main/kotlin/com/polarbookshop/service/edge/ServiceEdgeApplication.kt
@@ -0,0 +1,11 @@
+package com.polarbookshop.service.edge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceEdgeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServiceEdgeApplication>(*args)
+}
---
+++ src/test/kotlin/com/polarbookshop/service/edge/ServiceEdgeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.polarbookshop.service.edge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceEdgeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
