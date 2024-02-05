3b1b8ccef24f98d5fe43f8b1705bae473c9874dc
dmitry.ivzhenko <demi-drolll@yandex.ru>
2024-02-05 10:26:27 +0300

    config and ci/cd

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 }
 
 extra["springCloudVersion"] = "2023.0.0"
+extra["testcontainersVersion"] = "1.17.3"
 
 dependencies {
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
@@ -27,13 +28,17 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+	implementation("org.springframework.session:spring-session-data-redis")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
+	testImplementation("org.testcontainers:junit-jupiter")
 }
 
 dependencyManagement {
 	imports {
 		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+		mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
 	}
 }
 
@@ -46,4 +51,17 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.bootBuildImage {
+	imageName.set(project.name)
+	environment.set(mapOf("BP_JVM_VERSION" to "17.*"))
+
+	docker {
+		publishRegistry {
+			project.findProperty("registryUsername")?.let { username.set(it as String) }
+			project.findProperty("registryToken")?.let { password.set(it as String) }
+			project.findProperty("registryUrl")?.let { url.set(it as String) }
+		}
+	}
 }
--- src/test/kotlin/com/polarbookshop/service/edge/ServiceEdgeApplicationTests.kt
+++ src/test/kotlin/com/polarbookshop/service/edge/ServiceEdgeApplicationTests.kt
@@ -2,12 +2,39 @@ package com.polarbookshop.service.edge
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
 
-@SpringBootTest
+@SpringBootTest(
+	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Testcontainers
 class ServiceEdgeApplicationTests {
 
 	@Test
 	fun contextLoads() {
+	}
+
+	companion object {
+		private const val REDIS_PORT: Int = 6379
+
+		@Container
+		val redis: GenericContainer<*> = GenericContainer(DockerImageName.parse("redis:7.0"))
+			.withExposedPorts(REDIS_PORT)
+
+		@DynamicPropertySource
+		fun redisProperties(registry: DynamicPropertyRegistry) {
+			registry.add("spring.redis.host") {
+				redis.host
+			}
+			registry.add("spring.redis.port") {
+				redis.getMappedPort(REDIS_PORT)
+			}
+		}
 	}
 
 }
